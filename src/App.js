6a281ddfb792c9a88c0d08c1d1f91568045dfe6d
Daniel Romano <bydanielromano@gmail.com>
2022-10-12 14:03:36 +0200

    privates routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from './context/AuthContext';
 
 // import pages
 import Categories from './pages/Categories';
@@ -7,19 +8,22 @@ import Category from './pages/Category';
 import Videos from "./pages/Videos";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Unauthorized from './pages/Unauthorized';
 import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useContext(AuthContext);
+
   return (    
       <BrowserRouter>
         <Routes>
-          <PrivateRoute exact path="/" element={<Categories />} />
           <Route path='/' element={<Navigate replace to='/signup' />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/categories' element={<Categories />} />
-          <Route path='/:category' element={<Category />} />
-          <Route path='/:category/:id' element={<Videos />} />       
+          <Route path='/categories' element={currentUser ? <Categories /> : <Login />} />
+          <Route path='/:category' element={currentUser ? <Category /> : <Login />} />
+          <Route path='/:category/:id' element={currentUser ? <Videos /> : <Login />} />       
         </Routes>
       </BrowserRouter>
   );
